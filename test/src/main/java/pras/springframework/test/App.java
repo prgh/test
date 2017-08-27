9c73ee16c112ed8d45443b4b81694884e56aec17
prgh <pghattamaneni@gmail.com>
2017-08-27 11:01:33 -0700

    initial commit

---
+++ test/src/main/java/pras/springframework/test/App.java
@@ -0,0 +1,13 @@
+package pras.springframework.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
